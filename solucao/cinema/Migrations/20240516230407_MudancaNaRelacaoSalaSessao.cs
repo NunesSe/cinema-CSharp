b78987acfa007781f3271626d6ff19fb72c2e7ea
Felipe Nunes <felipenunesseixas@gmail.com>
2024-05-16 20:08:01 -0300

    Salas Cadastro e Listar funcionando

---
+++ solucao/cinema/Migrations/20240516230407_MudancaNaRelacaoSalaSessao.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cinema.Migrations
+{
+    /// <inheritdoc />
+    public partial class MudancaNaRelacaoSalaSessao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sessoes_SalaId",
+                table: "Sessoes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessoes_SalaId",
+                table: "Sessoes",
+                column: "SalaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sessoes_SalaId",
+                table: "Sessoes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessoes_SalaId",
+                table: "Sessoes",
+                column: "SalaId",
+                unique: true);
+        }
+    }
+}
